d76c04b447bf409e19574cd1a5122138d03e155f
jgzuke <jgzuke@gmail.com>
2015-03-26 00:28:37 -0400

    DrawingShapes works awesome

--- src/com/drawinggame/GestureDetector.java
+++ src/com/drawinggame/GestureDetector.java
@@ -1,13 +1,21 @@
 package com.drawinggame;
 
+import java.util.ArrayList;
+
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-public class GestureDetector implements OnTouchListener {
+public class GestureDetector implements OnTouchListener
+{
 	private MyView myView;
 	private double screenWidth;
 	private double screenHeight;
+	private ArrayList<Integer> IDs = new ArrayList<Integer>();
+	private ArrayList<ArrayList<int[]>> pointsLists = new ArrayList<ArrayList<int[]>>();
+	private int actionMask;
+	int[] coordinate = new int[2];
+	int ID = 0;
     public GestureDetector(MyView myViewSet)
     {
     	myView = myViewSet;
@@ -19,20 +27,64 @@ public class GestureDetector implements OnTouchListener {
 	@Override
     public boolean onTouch(View v, MotionEvent e)
 	{
-		int actionMask = e.getActionMasked();
+		actionMask = e.getActionMasked();
 		switch (actionMask)
 		{
-		    case MotionEvent.ACTION_DOWN:
-		    break;
-		    case MotionEvent.ACTION_MOVE:
-		    break;
-		    case MotionEvent.ACTION_UP:
-		    break;
-		    case MotionEvent.ACTION_POINTER_UP:
-		    break;
-		    case MotionEvent.ACTION_POINTER_DOWN:
-		    break;
+		case MotionEvent.ACTION_DOWN:
+			ArrayList<int[]> newPointSet = new ArrayList<int[]>();	// Start a new array of points
+			coordinate[0] = (int)(e.getX());						// Add current point to array
+        	coordinate[1] = (int)(e.getY());						
+			newPointSet.add(coordinate.clone());					// Add array to list
+			pointsLists.add(newPointSet);
+			
+        	ID = e.getPointerId(e.getActionIndex());				// Add pointer ID to ID list
+        	IDs.add(ID);
+        break;
+        case MotionEvent.ACTION_POINTER_DOWN:
+        	ID = e.getPointerId(e.getActionIndex());
+        	if(ID < e.getPointerCount())
+        	{
+	        	ArrayList<int[]> newAltPointSet = new ArrayList<int[]>();
+	        	coordinate[0] = (int)(e.getX(ID));
+	        	coordinate[1] = (int)(e.getY(ID));
+	        	newAltPointSet.add(coordinate.clone());
+	        	pointsLists.add(newAltPointSet);
+	        	
+	        	IDs.add(ID);
+        	}
+        break;
+        case MotionEvent.ACTION_MOVE:
+        	ID = e.getPointerId(e.getActionIndex());
+        	for(int i = 0; i < IDs.size(); i++)
+        	{
+        		if(ID == IDs.get(i))
+        		{
+	        		coordinate[0] = (int)(e.getX(ID));
+	        		coordinate[1] = (int)(e.getY(ID));
+	        		pointsLists.get(i).add(coordinate.clone());
+        		}
+        	}
+        break;
+        case MotionEvent.ACTION_UP:
+        	pointsLists.clear();
+        	IDs.clear();
+        break;
+        case MotionEvent.ACTION_POINTER_UP:
+        	ID = e.getPointerId(e.getActionIndex());
+        	for(int i = 0; i < IDs.size(); i++)
+        	{
+        		if(ID == IDs.get(i))
+	        	{
+        			IDs.remove(i);
+        			pointsLists.remove(i);
+	        	}
+        	}
+        break;
 		}
 		return true;
     }
+	protected ArrayList<ArrayList<int[]>> getPointsLists()
+	{
+		return pointsLists;
+	}
 }
--- src/com/drawinggame/MyView.java
+++ src/com/drawinggame/MyView.java
@@ -1,8 +1,15 @@
 package com.drawinggame;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Paint.Style;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 
 public class MyView extends View {
@@ -11,6 +18,7 @@ public class MyView extends View {
 	private ImageLibrary imageLibrary;
 	private GestureDetector gestureDetector;
 	private Handler mHandler = new Handler();
+	private Paint paint;
 	protected Runnable frameCaller = new Runnable()
 	{
 		public void run()
@@ -31,6 +39,8 @@ public class MyView extends View {
     	screenWidth = screenDimensions[0];
     	screenHeight = screenDimensions[1];
     	gestureDetector = new GestureDetector(this);
+    	paint = new Paint();
+    	frameCaller.run();
 		this.setOnTouchListener(gestureDetector);
     }
     private void frameCall()
@@ -40,6 +50,25 @@ public class MyView extends View {
     @Override
 	protected void onDraw(Canvas g)
 	{
+    	ArrayList<ArrayList<int[]>> pointsList = gestureDetector.getPointsLists();
+    	paint.setColor(Color.RED);
+    	paint.setStyle(Style.STROKE);
+    	paint.setStrokeWidth(4);
+    	Log.e("myid", "test");
+    	for(int i = 0; i < pointsList.size(); i++)
+    	{
+    		ArrayList<int[]> points = pointsList.get(i);
+    		Log.e("myid", Integer.toString(points.size()));
+    		Path path = new Path();
+    		path.moveTo(points.get(0)[0], points.get(0)[1]);
+    		for(int j = 1; j < points.size(); j++)
+    		{
+    			path.lineTo(points.get(j)[0], points.get(j)[1]);
+    		}
+    		g.drawPath(path, paint);
+    	}
+    	
+    	
 		//g.scale((float) screenDimensionMultiplier, (float) screenDimensionMultiplier);
 		//drawNotPaused(g);
 	}
